912e26d5a1512c3f424f06d709c4cb7b82c548b5
andrii <alltelex@gmail.com>
2025-08-27 15:06:32 +0300

    Dispose web driver on session terminating

--- src/main/java/ru/pechat55/constructor/render/Constructor.java
+++ src/main/java/ru/pechat55/constructor/render/Constructor.java
@@ -95,7 +95,7 @@ public class Constructor {
             return true;
         } catch (NoSuchSessionException | SessionNotCreatedException e) {
             log.info("Session not found, {}", driver.getSessionId());
-            driver.quit();
+            driver.close();
             return false;
         } catch (Exception e) {
             log.error("Unexpected session check error: {}", e.getMessage());
